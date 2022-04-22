c90703a4edbf4584f33619e5196c773272bdc963
Сергей Акимов <akimovserega@gmail.com>
2022-04-23 01:08:58 +0500

    task 1 is done

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,106 +1,66 @@
 package ru.netology
 
-const val MAESTRO_MASTER_NO_FEE_MIN = 300_00
-const val MAESTRO_MASTER_NO_FEE_MAX = 75_000_00
-const val MAESTRO_MASTER_FEE = 0.6F / 100
-const val MAESTRO_MASTER_CONST_FEE = 20_00
-const val MAESTRO_MASTER_TEMP_FEE = 0
-
-const val VISA_MIR_FEE = 0.75F / 100
-const val VISA_MIR_MIN_FEE = 35_00
-const val VK_PAY_FEE = 0
-
-const val VK_PAY_MAX_LIM = 15_000_00
-const val VK_PAY_MAX_MONTHLIM = 40_000_00
-const val CARD_LIM = 150_000_00
-const val CARD_MONTHLIM = 600_000_00
-
 
 fun main() {
 
-    // два вида карт и VK pay
-    var type: String = "Mastercard"
-    var monthAmount: Int = 100_000_00
-    var currentAmount: Int = 1_000_00
+// 6 вариантов:
 
+    var numSeconds: Int = 30
+    println(agoToText(numSeconds))
 
-    var fee = calculateFee(type, monthAmount, currentAmount)
-    printFee(type, currentAmount, monthAmount, fee)
+    numSeconds = 1850
+    println(agoToText(numSeconds))
 
-    monthAmount = 70_000_00
-    currentAmount = 15_000_00
-    fee = calculateFee(type, monthAmount, currentAmount)
-    printFee(type, currentAmount, monthAmount, fee)
+    numSeconds = 8936
+    println(agoToText(numSeconds))
 
-    type = "Visa"
-    monthAmount = 100_000_00
-    currentAmount = 15_000_00
-    fee = calculateFee(type, monthAmount, currentAmount)
-    printFee(type, currentAmount, monthAmount, fee)
+    numSeconds = 150_123
+    println(agoToText(numSeconds))
 
-    monthAmount = 10_000_00
-    currentAmount = 1_000_00
-    fee = calculateFee(type, monthAmount, currentAmount)
-    printFee(type, currentAmount, monthAmount, fee)
+    numSeconds = 187_781
+    println(agoToText(numSeconds))
 
-
-    currentAmount = 15_000_00
-    fee = calculateFee(currentAmount = currentAmount)
-    printFee(amount = currentAmount, fee = fee)
-
-    monthAmount = 30_000_00
-    currentAmount = 15_000_00
-    fee = calculateFee(monthAmount = monthAmount, currentAmount = currentAmount)
-    printFee(amount = currentAmount, total = monthAmount, fee = fee)
+    numSeconds = 1_161_165
+    println(agoToText(numSeconds))
 
 }
 
-fun calculateFee(cardType: String = "Vk Pay", monthAmount: Int = 0, currentAmount: Int): Int {
-    var fee: Int = 0;
 
-    if (!checkLimits(cardType, monthAmount, currentAmount)) {
-        //Превышен лимит на перевод
+fun agoToText(numSeconds: Int): String {
 
-        return -1
+    var retString: String = "был(а) в сети "
+    val recently: String = "был(а) только что"
 
-    } else {
-        return when (cardType) {
-            "Mastercard", "Maestro" ->
-                if (currentAmount >= MAESTRO_MASTER_NO_FEE_MIN && currentAmount + monthAmount <= MAESTRO_MASTER_NO_FEE_MAX)
-                    MAESTRO_MASTER_TEMP_FEE
-                else (currentAmount * MAESTRO_MASTER_FEE + MAESTRO_MASTER_CONST_FEE).toInt()
-            "Visa", "Mir" ->
-                if (currentAmount * VISA_MIR_FEE > VISA_MIR_MIN_FEE) (currentAmount * VISA_MIR_FEE).toInt()
-                else VISA_MIR_MIN_FEE
-            "Vk Pay" -> VK_PAY_FEE
-            else -> 0
-        }
+    // условия по ДЗ
+    when (numSeconds) {
+        in 0..60 -> retString = recently
+        in 61..60 * 60 -> retString += "${numSeconds / 60} ${minuteFormat(numSeconds / 60)} назад"
+        in 60 * 60 + 1..24 * 60 * 60 -> retString += "${numSeconds / 3600} ${hourFormat(numSeconds / 3600)} назад"
+        in 24 * 60 * 60 + 1..2 * 24 * 60 * 60 -> retString += "сегодня"
+        in 2 * 24 * 60 * 60 + 1..3 * 24 * 60 * 60 -> retString += "вчера"
+        else -> retString += "давно"
+    }
+
+    return retString
+}
+
+
+fun minuteFormat(num: Int): String {
+    // -ы, -у. From 1 to 60
+    return when (num % 10) {
+        1 -> "минуту"
+        in 2..4 -> "минуты"
+        in 5..9, 0 -> "минут"
+        else -> "мин."
     }
 }
 
-
-fun checkLimits(cardType: String = "Vk Pay", monthAmount: Int = 0, currentAmount: Int): Boolean {
-
-    return when (cardType) {
-        "Mastercard", "Maestro", "Visa", "Mir" ->
-            (currentAmount <= CARD_LIM) && (monthAmount + currentAmount <= CARD_MONTHLIM)
-        "Vk Pay" -> (currentAmount <= VK_PAY_MAX_LIM) && (monthAmount + currentAmount <= VK_PAY_MAX_MONTHLIM)
-        else -> {
-            true
-        }
+fun hourFormat(num: Int): String {
+    //-а, -ов From 1 to 24
+    return when (num % 10) {
+        1 -> "час"
+        in 2..4 -> "часа"
+        in 5..9, 0 -> "часов"
+        else -> "час."
     }
-}
-
-fun printFee(type: String = "Vk Pay", amount: Int, total: Int = 0, fee: Int): Boolean {
-    println(
-        "Выбрана платежная система: $type. Сумма перевода  ${centFormat(amount)}, сумма предыдущих" +
-                " переводов ${centFormat(total)}, " +
-                "${if (fee >= 0) "итого Ваша комиссия составляет ${centFormat(fee)}" else "превышен лимит на перевод"}"
-    )
-
-    return true
-}
-
-fun centFormat(amount: Int): String {
-    return if (amount % 100 > 0) "${amount / 100}.${amount % 100}" else "${amount / 100}"
 }
